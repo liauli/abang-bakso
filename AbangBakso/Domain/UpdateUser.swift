5a288a499d7541d8eede38ec3871460972d7520a
aulia_nastiti <aulia_nastiti@telkomsel.co.id>
2024-11-13 01:44:02 +0700

    [#30] Send seller location - Create update user usecase to update location

---
+++ AbangBakso/Domain/UpdateUser.swift
@@ -0,0 +1,25 @@
+//
+//  UpdateUser.swift
+//  AbangBakso
+//
+//  Created by aulia_nastiti on 13/11/24.
+//
+
+import Combine
+import Foundation
+
+protocol UpdateUser: AutoMockable {
+    func execute(user: User) -> AnyPublisher<Void, FirestoreError>
+}
+
+class UpdateUserImpl: UpdateUser {
+    private let userRepository: UserRepository
+    
+    init(_ userRepository: UserRepository) {
+        self.userRepository = userRepository
+    }
+    
+    func execute(user: User)  -> AnyPublisher<Void, FirestoreError>{
+        return userRepository.update(user: user)
+    }
+}
